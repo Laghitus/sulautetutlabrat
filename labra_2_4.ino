d354893f1b7a283fb76ab2e62876c66e6de41d4b
Joni Pokka <jonipokka@hotmail.com>
2017-12-14 16:57:20 +0200

    Update labra_2_4.ino

--- labra_2_4.ino
+++ labra_2_4.ino
@@ -1,22 +1,40 @@
-#include "TimerOne.h"
-int analogValue;
- 
-void setup()
-{
-  pinMode(A1, INPUT);
-  Timer1.initialize(1000); 
-  Timer1.attachInterrupt(callback);
+// Muuttujat
+int analogValue1;
+int analogValue2;
+float resistance;
+
+void setup(){
+  
+  // Pinnit
+  pinMode(A0,INPUT);
+  pinMode(A1,INPUT);
+  Serial.begin(9600);
+
+  cli();
+
+  TCCR0A = 0;// set entire TCCR2A register to 0
+  TCCR0B = 0;// same for TCCR2B
+  TCNT0  = 0;//initialize counter value to 0
+  OCR0A = 124;// = (16*10^6) / (2000*64) - 1 (must be <256)
+  TCCR0A |= (1 << WGM01);
+  TCCR0B |= (1 << CS01) | (1 << CS00);   
+  TIMSK0 |= (1 << OCIE0A);
+
+  sei();
+
 }
- 
-void callback()
-{
-  analogValue = analogRead(A1);
-  Serial.print("RX arvo: ");
-  Serial.println(analogValue);
+
+ISR(TIMER0_COMPA_vect){
+  analogValue1 = analogRead(A0);
+  analogValue2 = analogRead(A1);
+  resistance = (19.168*(float)analogValue2)-334.06;
+  Serial.print(analogValue2);
+  Serial.print("  ");
+  Serial.print("Resistance: ");
+  Serial.println(resistance);
 }
- 
-void loop()
-{
-  // your program here...
+
+
+void loop(){
+  //do other things here
 }
- 
